1247e47113834d8bfc9d524abfee1781cc54ebe8
lorenzo magnoni <lorenzo.magnoni@studenti.unimi.it>
2024-03-06 09:42:04 +0100

    csv reorganization

--- src/lib/input/read_R.hpp
+++ src/lib/input/read_R.hpp
@@ -2,12 +2,15 @@
 #define __READ_R_HPP__
 
 #include "../utility/2D_vector.hpp"
-#include "../csv_reading/csv_reader.hpp"
+#include "../csv/csv.hpp"
 #include "parsing.hpp"
 #include <vector> //vector
 #include <string> //string
 #include <fstream>
 #include <stdexcept>
+
+
+using namespace csv;
 
 const std::string DEFAULT_FILENAME = "./data/R.csv";
 
